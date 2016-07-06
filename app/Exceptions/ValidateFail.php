dbd3238803863aaad15e734f6ea6dc1aa85d666c
chivincent <song374561@gmail.com>
2016-07-06 13:31:19 +0800

    Create main function for this feature.

---
+++ app/Exceptions/ValidateFail.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace SmartCafe\Exceptions;
+
+use Exception;
+use Illuminate\Support\MessageBag;
+
+class ValidateFail extends Exception
+{
+    public function __construct(MessageBag $message, int $code = 0, Exception $previous = null)
+    {
+        $message = json_encode($message->toArray());
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function getErrors(): MessageBag
+    {
+        $errors = json_decode($this->message);
+        $messageBag = new MessageBag((array)$errors);
+        return $messageBag;
+    }
+}
